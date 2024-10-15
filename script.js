a065ba9d54e568107d57e5d28707bcc83a7f7ebf
atrajit-sarkar <atrajit.sarkar@gmail.com>
2024-10-15 17:16:28 +0530

    up

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Paste the link of your mark down file here to render it into your site
-const myMarkDownFile="https://raw.githubusercontent.com/Gongo-Bongo/C-cheatsheet/refs/heads/main/C-cheatsheet.md"
+const myMarkDownFile="https://raw.githubusercontent.com/Gongo-Bongo/C-S-Inequality/refs/heads/main/cs-inequality.md"
 
 // Fetch the Markdown file and render it
 async function getmd() {
